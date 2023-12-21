9d5f73ae4f3450080cd875ba9b77a7beb7ad464b
Rodrigo Vinicius Leandrin <108472050+RodrigoLeandrin@users.noreply.github.com>
2023-12-20 23:18:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var feild = document.querySelector('textarea');
-var backup = document.querySelector('placeholder');
-var button = document.querySelector('.button');
+const field = document.querySelector('textarea');
+const submit = document.querySelector('#submit');
+const comments_arr = [];
+const comments = document.getElementById('comment-box');
 
-feild.onfocus = function(){
-    button.style.display = 'display';
+
+
+const display_comments =() => {
+    let list = '<ul>';
+       comments_arr.forEach(comments => {
+        list += `<li>{comments}</li>`;
+      })
+      list += '</ul>';
+      comments.innerHTML = list;}
+
+submit.onclick = function () {
+    const content = field.value;
+    comments_arr.push(content);
+    console.log(comments_arr);
+    display_comments ();
 }
+
